376db68b000865d5778f2a4bc61baf55a66d8747
Bex-Li <rebekkikko@gmail.com>
2018-04-05 22:42:17 -0700

    updates to gifHW.js and index.html

--- assets/js/gifHW.js
+++ assets/js/gifHW.js
@@ -1,26 +1,50 @@
 console.log("hello");
 
 var animals = [
-    "DOGS",
-    "CATS", 
-    "PIGS",
-    "CHICKENS"
-]
+    "dogs",
+    "cats",
+    "pigs",
+    "chickens"
+];
 
 //create for loop for button
 for (var i = 0; i < animals.length; i++) {
     var button = $('<button/>').text(animals[i]);
-    button.addClass( "animalButtons" );
-    $(".animalButDiv").append(button)
+    button.addClass("animalButtons");
+    $(".animalButDiv").append(button);
     //var parent = $('<tr><td></td></tr>').children().append(test).end();
 }
 
-$(document).on("click", ".animalButtons", function(event) {
+$(document).on("click", ".animalButtons", function (event) {
     console.log("buttons!");
     console.log(event.target.innerText);
-    //use this variable event.target.innerText with the AJAX request so the AJAX can send to the GIPHY API using selectAnimal
-    var selectAnimal = event.target.innerText 
+    var selectAnimal = event.target.innerText;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=5&q=";
+    queryURL += selectAnimal
 
-    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // console.log('response =====>', response)
+        // console.log('response.data =====>', response.data);
+
+        response.data.forEach(function (gifObject) {
+            var img = $('<img>')
+
+            img.attr('src', gifObject.images.fixed_height.url)
+
+            $('#gif-holder').prepend(img)
+        })
+    });
 });
+
+$("#pickerButton").on("click", function(){
+    var inputVal = $("#newInput").val().trim();
+    var button = $('<button/>').text(inputVal);
+    button.addClass("animalButtons");
+    $(".animalButDiv").append(button);
+})
+
+
 
